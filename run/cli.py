df7e99374dd7ddf9869bc13f18fe80f2fae0192b
roll <eskarev@gmail.com>
2017-12-05 10:52:53 +0300

    Minor refactoring

--- run/cli.py
+++ run/cli.py
@@ -12,16 +12,18 @@ from . import helpers
 # Main program
 
 if __name__ == '__main__':
+
+    # Arguments
     argv = sys.argv[1:]
 
-    # Path
+    # Path argument
     path = 'run.yml'
     if '--run-path' in argv:
         index = argv.index('--run-path')
         path = argv.pop(index + 1)
         argv.pop(index)
 
-    # Complete
+    # Complete argument
     complete = False
     if '--run-complete' in argv:
         argv.remove('--run-complete')
@@ -31,8 +33,10 @@ if __name__ == '__main__':
     config, options = helpers.read_config(path)
     task = Task(config, options=options)
 
-    # Run
+    # Complete
     if complete:
         task.complete(argv)
         exit()
+
+    # Run
     task.run(argv)
